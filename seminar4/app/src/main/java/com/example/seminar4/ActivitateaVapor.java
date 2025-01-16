38323f0ce2497f7ea389766fac571b6c4838587e
ValentinaRadoi <radoivalentina22@stud.ase.ro>
2025-01-16 14:42:31 +0200

    seminar 4

---
+++ seminar4/app/src/main/java/com/example/seminar4/ActivitateaVapor.java
@@ -0,0 +1,71 @@
+package com.example.seminar4;
+
+import static android.app.ProgressDialog.show;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioGroup;
+import android.widget.RatingBar;
+import android.widget.Spinner;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class ActivitateaVapor extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_activitatea_vapor);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+
+        });
+
+        Button btnVapor = findViewById(R.id.btn_const_vapor);
+        btnVapor.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText denumire = findViewById(R.id.editTextText2);
+                Spinner spin=findViewById(R.id.spinner2);
+                EditText detalii = findViewById(R.id.editTextText3);
+                RadioGroup rdg=findViewById(R.id.rGROUP);
+                RatingBar rtg=findViewById(R.id.ratingBar);
+                int selectedID=rdg.getCheckedRadioButtonId();
+
+                int alesSpin= Integer.parseInt(spin.getSelectedItem().toString());
+                String denumireString=denumire.getText().toString();
+                String detaliiString=detalii.getText().toString();
+                boolean curat=false;
+                if(selectedID==R.id.radio_de_persoane)
+                {
+                    curat=true;
+                }
+                float grade=rtg.getRating()+30;
+
+                Vapor v1 = new Vapor(denumireString,alesSpin,detaliiString,curat,grade);
+
+                Log.d("#obiect",v1.toString());
+
+                Intent intent = new Intent();
+                intent.putExtra("vapor",v1);
+                setResult(RESULT_OK,intent);
+                finish();
+
+            }
+        });
+
+    }
+
+
+}
---
+++ seminar4/app/src/main/java/com/example/seminar4/MainActivity.java
@@ -0,0 +1,61 @@
+package com.example.seminar4;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TableRow;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ArrayList<Vapor>vaporList=new ArrayList<>();
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        Button btn=findViewById(R.id.button);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent it = new Intent(getApplicationContext(), ActivitateaVapor.class);
+                startActivityForResult(it,200);
+            }
+        });
+
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == 200 && resultCode == RESULT_OK && data != null)
+        {
+            Vapor v = data.getParcelableExtra("vapor");
+            if(v!=null)
+            {
+                vaporList.add(v);
+                Toast.makeText(this, "Obiectul a fost adaugat:"+ v.toString(), Toast.LENGTH_SHORT).show();
+
+            }
+
+        }
+    }
+}
